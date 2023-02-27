3333839fc1d3b180c7869781a9a9ac3f6e805464
Terry Cater <baconfield@protonmail.com>
2023-02-27 03:21:53 -0600

    Unify syncthing setup under ./services/syncthing.nix
    Unified synthing settings under ./services/syncthing.nix and refactored elsewhere accordingly

--- systems/DoveTrail/DoveTrail.nix
+++ systems/DoveTrail/DoveTrail.nix
@@ -8,8 +8,8 @@
 
   networking = {
     hostName = "DoveTrail";
-    firewall.allowedTCPPorts = [ 853 3000 5600 6700 ];
-    firewall.allowedUDPPorts = [ 53 ];
+    firewall.allowedTCPPorts = [ 853 3000 5600 6700 8384 22000 ];
+    firewall.allowedUDPPorts = [ 53 22000 ];
   };
 
   containers.adguardhome = {
@@ -33,6 +33,22 @@
       system.stateVersion = "22.11";
       environment.etc."resolv.conf".text = "nameserver 8.8.8.8";
     };
+  };
+
+  services.syncthing = { # Uses ports 8384 and 22000
+    guiAddress = "10.0.1.20:8384";
+    devices = {
+      "FoxSummit" = { id = "[REDACTED]"; };
+      "TortiseCove" = { id = "[REDACTED]"; };
+    };
+    folders =
+      let devices = [ "FoxSummit" "TortiseCove" ]
+      in {
+        "Documents".devices = devices;
+        "Music".devices = devices;
+        "Pictures".devices = devices;
+        "Videos".devices = devices;
+      };
   };
 
   systemd.services.container@uptime-kuma.Unit.After = "network.target";
