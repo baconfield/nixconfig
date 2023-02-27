32ff5d3b6d50cccf032b6f38a52e188f1e0b0a50
Terry Cater <baconfield@protonmail.com>
2023-02-27 02:57:27 -0600

    Refactor TortiseCove.nix
    Refactored the services section of TortiseCove.nix for readability

--- systems/TortiseCove/TortiseCove.nix
+++ systems/TortiseCove/TortiseCove.nix
@@ -51,56 +51,55 @@
       };
   };
 
-  services = { # Uses port 6443
-    k3s = {
-      enable = true;
-      role = "server";
-      extraFlags = toString [
-        "--write-kubeconfig-mode --container-runtime-endpoint unix:///run/containerd/containerd.sock"
-      ];
-    };
-    syncthing = { # Uses ports 8384, 22000, and 21027
-      enable = true;
-      user = "tjcater";
-      dataDir = "/filepit"; # Default folder for newly synced folders
-      configDir = "/home/tjcater/.config/syncthing";
-      guiAddress = "0.0.0.0:8384";
-      devices = {
-        "FoxSummit" = { id = "[REDACTED]"; };
-        "DoveTrail" = { id = "[REDACTED]"; }; # Need to replace id once reestablished
-      };
-      folders = {
-        "Documents" = {
-          id = "[REDACTED]";
-          path = "/filepit/documents";
-          devices = [ "FoxSummit" "DoveTrail" ];
-        };
-        "Music" = {
-          id = "[REDACTED]";
-          path = "/filepit/music";
-          devices = [ "FoxSummit" "DoveTrail" ];
-        };
-        "Pictures" = {
-          id = "[REDACTED]";
-          path = "/filepit/pictures";
-          devices = [ "FoxSummit" "DoveTrail" ];
-        };
-        "Videos" = {
-          id = "[REDACTED]";
-          path = "/filepit/videos";
-          devices = [ "FoxSummit" "DoveTrail" ];
-        };
-      };
+  services.k3s = { # Uses port 6443
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      "--write-kubeconfig-mode --container-runtime-endpoint unix:///run/containerd/containerd.sock"
+    ];
+  };
 
+  services.syncthing = { # Uses ports 8384, 22000, and 21027
+    enable = true;
+    user = "tjcater";
+    dataDir = "/filepit"; # Default folder for newly synced folders
+    configDir = "/home/tjcater/.config/syncthing";
+    guiAddress = "0.0.0.0:8384";
+    devices = {
+      "FoxSummit" = { id = "[REDACTED]"; };
+      "DoveTrail" = { id = "[REDACTED]"; }; # Need to replace id once reestablished
     };
-    udev.extraRules = ''
+    folders = {
+      "Documents" = {
+        id = "[REDACTED]";
+        path = "/filepit/documents";
+        devices = [ "FoxSummit" "DoveTrail" ];
+      };
+      "Music" = {
+        id = "[REDACTED]";
+        path = "/filepit/music";
+        devices = [ "FoxSummit" "DoveTrail" ];
+      };
+      "Pictures" = {
+        id = "[REDACTED]";
+        path = "/filepit/pictures";
+        devices = [ "FoxSummit" "DoveTrail" ];
+      };
+      "Videos" = {
+        id = "[REDACTED]";
+        path = "/filepit/videos";
+        devices = [ "FoxSummit" "DoveTrail" ];
+      };
+    };
+  };
+
+  services.udev.extraRules = ''
       ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
     '';
 
-    zfs = {
-      autoScrub.enable = true;
-      trim.enable = true;
-    };
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
