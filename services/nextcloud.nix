0114b386db72dc5223ab2d193a0567845e406af7
Terry Cater <baconfield@protonmail.com>
2023-05-06 05:47:19 -0500

    Refactor configs

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -1,45 +1,50 @@
 { config, pkgs, ... }:
 {
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud26;
+  services = {
+    nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud26;
 
-    https = true;
-    config = {
-      dbtype = "pgsql";
-      dbuser = "nextcloud";
-      dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-      dbname = "nextcloud";
-      adminuser = "root";
-    };
-    caching.redis = true;
-    caching.apcu = false;
-    extraOptions = {
-      redis = {
-        host = "/run/redis-nextcloud/redis.sock";
-        port = 0;
+      https = true;
+      config = {
+        dbtype = "pgsql";
+        dbuser = "nextcloud";
+        dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
+        dbname = "nextcloud";
+        adminuser = "root";
       };
-      memcache = {
-        local = "\\OC\\Memcache\\Redis";
-        distributed = "\\OC\\Memcache\\Redis";
-        locking = "\\OC\\Memcache\\Redis";
+      caching.redis = true;
+      caching.apcu = false;
+      extraOptions = {
+        redis = {
+          host = "/run/redis-nextcloud/redis.sock";
+          port = 0;
+        };
+        memcache = {
+          local = "\\OC\\Memcache\\Redis";
+          distributed = "\\OC\\Memcache\\Redis";
+          locking = "\\OC\\Memcache\\Redis";
+        };
       };
     };
+
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "nextcloud" ];
+      ensureUsers = [
+      { name = "nextcloud";
+        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+      }
+      ];
+    };
+
+    redis.servers.nextcloud = {
+      enable = true;
+      user = "nextcloud";
+      port = 0;
+    };
   };
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [
-    { name = "nextcloud";
-      ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-    }
-    ];
-  };
-  services.redis.servers.nextcloud = {
-    enable = true;
-    user = "nextcloud";
-    port = 0;
-  };
+
   systemd.services."nextcloud-setup" = {
     requires = ["postgresql.service"];
     after = ["postgresql.service"];
