e53ba5b687b5d4362f2991b4b2a0cac3e8a144f8
Terry Cater <baconfield@protonmail.com>
2023-09-30 13:48:31 -0500

    Add Syncthing id

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -8,13 +8,15 @@
     overrideFolders = true;
     
     settings.devices = {
-      "DoveTrail"   = { id = "RSSLIGN-J6CDKVB-XRL3I4Z-C74QBSK-FHV4OY2-C2MZ575-VGHCQUF-NEZ55Q5"; };
-      "FoxSummit"   = { id = "23UFPLY-ATX7XAE-ODKE76T-IGF76JG-P5IGCGY-MJHAXRN-MTGOZGO-NY5HJAS"; };
+      "DoveTrail"     = { id = "RSSLIGN-J6CDKVB-XRL3I4Z-C74QBSK-FHV4OY2-C2MZ575-VGHCQUF-NEZ55Q5"; };
+      "FoxSummit"     = { id = "23UFPLY-ATX7XAE-ODKE76T-IGF76JG-P5IGCGY-MJHAXRN-MTGOZGO-NY5HJAS"; };
+      "RaccoonRapids" = { id = "333AKS2-53ZSTL5-BNIGK7R-KBAIVAP-7FFRZ2Q-L2ROK57-BT34JG5-ID7JFQ5"; };
     };
     settings.folders =
       let devices = [
         "DoveTrail"
         "FoxSummit"
+        "RaccoonRapids"
       ];
       in {
         "Documents" = {
