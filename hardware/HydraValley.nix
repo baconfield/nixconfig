673246a724c17841f0a1502583eee9a3c281e7e1
Kaiju Bacon <baconfield@protonmail.com>
2024-08-08 01:28:16 -0500

    Update flakes and enable syncthing on HydraValley

--- hardware/HydraValley.nix
+++ hardware/HydraValley.nix
@@ -1,26 +1,26 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "uas" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/786082e7-4e3b-45d7-a66b-a4a6b5baebbd";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/786082e7-4e3b-45d7-a66b-a4a6b5baebbd";
+    fsType = "ext4";
+  };
 
   boot.initrd.luks.devices."luks-8e6688e6-ffa7-4337-bcf3-8a241325295a".device = "/dev/disk/by-uuid/8e6688e6-ffa7-4337-bcf3-8a241325295a";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DA8B-EC38";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/DA8B-EC38";
+    fsType = "vfat";
+  };
 
   swapDevices = [ ];
 
