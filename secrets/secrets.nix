d0ebf7dbad6985f3bd4f3d5dfab4314fdf2a907e
Kaiju Bacon <baconfield@protonmail.com>
2024-08-08 01:28:16 -0500

    Add Proxmox setup and refactor README

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,10 @@
 # secrets/secrets.nix
 
 let
-    BaconField      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMluvcE2E0KZNFjBPidNOE+GAFsyjGWeBeO7tBYAnkWT";
     Foxsummit       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTJTJ8rSJhyCBbev272hIq1JyD22OF5kOheBVo6z6OC";
-    TortiseCove     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILC7uVOlqHKRxQiGFwvXWoVGfOkjKdXyFAvY3rWtnh/h";
-    DoveTrail       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQysvK01pWVVxeYJ0e/TCBmJ9rotds/GhzLLfsPChfK";
-    RaccoonRapids   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFh6+nX/vmyP5MSijcbL3Dv5NBI1hllZ396I45xnhpLr";
-    systems = [ Foxsummit TortiseCove DoveTrail RaccoonRapids ];
+    systems = [ Foxsummit ];
 in
 {
-    "credentials.age".publicKeys = [TortiseCove Foxsummit];
-    "nextcloudPass.age".publicKeys = [TortiseCove Foxsummit BaconField];
+    "credentials.age".publicKeys = [ Foxsummit];
+    "nextcloudPass.age".publicKeys = [ Foxsummit ];
 } # nix run github:ryantm/agenix -- -e credentials.age
