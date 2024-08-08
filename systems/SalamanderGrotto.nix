16e0ccac7b5c250e16d51e8891e21b0b7845fb76
Kaiju Bacon <baconfield@protonmail.com>
2024-08-08 01:28:16 -0500

    Update deluge config

--- systems/SalamanderGrotto.nix
+++ systems/SalamanderGrotto.nix
@@ -2,17 +2,21 @@
 
 {
   networking = {
-    firewall.allowedTCPPorts = [ 5055 5600 7878 8989 ];
+    firewall.allowedTCPPorts = [ 5055 5600 7878 8112 8989 ];
     firewall.allowedUDPPorts = [ ];
   };
   
+  services.deluge = {
+    enable = true;
+    web.enable = true; # Port 8112
+    datadir = "/filepit/deluge";
+  };
   services.jellyseer.enable = true; # Port 5055
   services.radarr.enable = true; # Port 7878
   services.sonarr.enable = true; # Port 8989
 
   environment.systemPackages = with pkgs; [
     btop
-    deluged
     fishPlugins.tide
     git
     gping
