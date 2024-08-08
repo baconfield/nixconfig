73679972b3a926694826da585dd42a29919ed99f
Kaiju Bacon <baconfield@protonmail.com>
2024-08-08 01:28:16 -0500

    Remove unused imports

--- systems/TanukiGrove.nix
+++ systems/TanukiGrove.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
-
   networking = {
     hostName = "TanukiGrove";
     firewall.allowedTCPPorts = [ 5600 8384 22000 ];
--- systems/TortiseCove.nix
+++ systems/TortiseCove.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
-
   boot = {
     initrd.supportedFilesystems = [ "zfs" ];
     supportedFilesystems = [ "zfs" ];
