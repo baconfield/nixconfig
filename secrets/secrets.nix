cb73473f985eda186b8cd7b7462f7feb958b9c75
Kaiju Bacon <baconfield@protonmail.com>
2024-08-08 01:28:16 -0500

    Reformat Pubkeys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,11 @@
 # secrets/secrets.nix
 
 let
-    BaconField = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMluvcE2E0KZNFjBPidNOE+GAFsyjGWeBeO7tBYAnkWT";
-    Foxsummit = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTJTJ8rSJhyCBbev272hIq1JyD22OF5kOheBVo6z6OC";
-    TortiseCove = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILC7uVOlqHKRxQiGFwvXWoVGfOkjKdXyFAvY3rWtnh/h";
-    DoveTrail = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQysvK01pWVVxeYJ0e/TCBmJ9rotds/GhzLLfsPChfK";
-    RaccoonRapids = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFh6+nX/vmyP5MSijcbL3Dv5NBI1hllZ396I45xnhpLr";
+    BaconField      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMluvcE2E0KZNFjBPidNOE+GAFsyjGWeBeO7tBYAnkWT";
+    Foxsummit       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTJTJ8rSJhyCBbev272hIq1JyD22OF5kOheBVo6z6OC";
+    TortiseCove     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILC7uVOlqHKRxQiGFwvXWoVGfOkjKdXyFAvY3rWtnh/h";
+    DoveTrail       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQysvK01pWVVxeYJ0e/TCBmJ9rotds/GhzLLfsPChfK";
+    RaccoonRapids   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFh6+nX/vmyP5MSijcbL3Dv5NBI1hllZ396I45xnhpLr";
     systems = [ Foxsummit TortiseCove DoveTrail RaccoonRapids ];
 in
 {
