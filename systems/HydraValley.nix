88449a35900fa09d2aadbe885f77bf919f56c64b
Kaiju Bacon <baconfield@protonmail.com>
2025-03-09 18:30:37 -0500

    Update flakes, add NH util

--- systems/HydraValley.nix
+++ systems/HydraValley.nix
@@ -24,6 +24,13 @@
     vim
   ];
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/tjcater/nixconfig";
+  };
+
   virtualisation.docker.enable = true;
   users.users.tjcater.extraGroups = [ "docker" ];
 
