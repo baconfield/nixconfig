ce62b2d7500c1f699d146860a98d679effebe723
Kaiju Bacon <baconfield@protonmail.com>
2025-11-29 03:57:28 -0600

    Update to pinentryPackage

--- users.nix
+++ users.nix
@@ -4,7 +4,7 @@
   programs.fish.enable = true;
   programs.gnupg.agent = {
      enable = true;
-     pinentryFlavor = "curses";
+     pinentryPackage = pkgs.pinentry-curses;
      enableSSHSupport = true;
   };
 
